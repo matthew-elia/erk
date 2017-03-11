bbef40944c5be58c004525ba00a92997e69daaf1
quincy-baker <m4tth3w.3l1a@gmail.com>
2017-03-11 17:16:41 -0500

    foundation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name erkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the erkApp
+ */
+angular.module('erkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
